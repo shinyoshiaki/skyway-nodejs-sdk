9fa02f36519188bf55af3caf107fde00db687903
shinyoshiaki <shinyoshiaki2012@gmail.com>
2023-11-10 10:37:43 +0900

    cleanup

--- packages/core/jest.config.js
+++
@@ -1,9 +0,0 @@
-/** @type {import('jest').Config} */
-const config = {
-  preset: 'ts-jest',
-  setupFiles: ['./jest.setup.js'],
-  testEnvironment: 'jsdom',
-  testRegex: 'tests/jest/.*\\.test\\.ts$',
-};
-
-module.exports = config;
