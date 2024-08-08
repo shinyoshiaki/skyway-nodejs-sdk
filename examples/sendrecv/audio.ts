d1d626b8b8955aa045aef373ef20f9484e4a33b3
shinyoshiaki <shinyoshiaki2012@gmail.com>
2024-08-08 15:20:51 +0900

    improve example

--- examples/sendrecv/audio.ts
+++ examples/sendrecv/audio.ts
@@ -1,15 +1,12 @@
 import {
-  Event,
-  LocalAudioStream,
-  MediaStreamTrack,
+  MediaStreamTrackFactory,
   RemoteVideoStream,
   RtpPacket,
   SkyWayContext,
   SkyWayRoom,
-  randomPort,
+  SkyWayStreamFactory,
 } from '../../packages/room/src';
 import { testTokenString } from './fixture';
-import { createSocket } from 'dgram';
 import Gst from '@girs/node-gst-1.0';
 import {
   deserializeAudioLevelIndication,
@@ -23,34 +20,32 @@ gst.init([]);
   const context = await SkyWayContext.Create(testTokenString, {
     codecCapabilities: [{ mimeType: 'audio/opus' }],
   });
+  const [track, port] = await MediaStreamTrackFactory.rtpSource({
+    kind: 'audio',
+    cb: (buf) => {
+      const rtp = RtpPacket.deSerialize(buf);
+      rtp.header.extension = true;
+      rtp.header.extensions.push({
+        id: 3,
+        payload: serializeAudioLevelIndication(25),
+      });
+      return rtp.serialize();
+    },
+  });
+  Gst.parseLaunch(
+    `audiotestsrc wave=ticks ! audioconvert ! audioresample ! queue ! opusenc ! rtpopuspay ! udpsink host=127.0.0.1 port=${port}`
+  ).setState(Gst.State.PLAYING);
+  SkyWayStreamFactory.registerMediaDevices({ audio: track });
+
   const room = await SkyWayRoom.Create(context, {
     type: 'sfu',
   });
   console.log('roomId', room.id);
   const sender = await room.join();
 
-  const audio = await randomPort();
-  const onAudio = new Event<Buffer>();
-  createSocket('udp4')
-    .on('message', (buf) => {
-      onAudio.emit(buf);
-    })
-    .bind(audio);
-  Gst.parseLaunch(
-    `audiotestsrc wave=ticks ! audioconvert ! audioresample ! queue ! opusenc ! rtpopuspay ! udpsink host=127.0.0.1 port=${audio}`
-  ).setState(Gst.State.PLAYING);
-  const audioTrack = new MediaStreamTrack({ kind: 'audio' });
-  onAudio.add((buf) => {
-    const rtp = RtpPacket.deSerialize(buf);
-    rtp.header.extension = true;
-    rtp.header.extensions.push({
-      id: 3,
-      payload: serializeAudioLevelIndication(25),
-    });
-    audioTrack.writeRtp(rtp);
-  });
-
-  const publication = await sender.publish(new LocalAudioStream(audioTrack));
+  const publication = await sender.publish(
+    await SkyWayStreamFactory.createMicrophoneAudioStream()
+  );
 
   const receiver = await (await SkyWayRoom.Find(context, room, 'sfu')).join();
   const { stream: remoteStream } = await receiver.subscribe<RemoteVideoStream>(
--- examples/sendrecv/video.ts
+++ examples/sendrecv/video.ts
@@ -1,15 +1,12 @@
 import {
-  LocalVideoStream,
-  MediaStreamTrack,
   RemoteVideoStream,
   SkyWayContext,
   SkyWayRoom,
   dePacketizeRtpPackets,
-  randomPort,
-  Event,
+  MediaStreamTrackFactory,
+  SkyWayStreamFactory,
 } from '../../packages/room/src';
 import { testTokenString } from './fixture';
-import { createSocket } from 'dgram';
 import Gst from '@girs/node-gst-1.0';
 
 const gst = require('node-gtk').require('Gst', '1.0') as typeof Gst;
@@ -27,32 +24,24 @@ gst.init([]);
         },
       },
     ],
-    rtcConfig: { turnPolicy: 'disable' },
   });
+  const [track, port] = await MediaStreamTrackFactory.rtpSource({
+    kind: 'video',
+  });
+  Gst.parseLaunch(
+    `videotestsrc ! video/x-raw,width=640,height=480,format=I420 ! x264enc key-int-max=60 ! rtph264pay ! udpsink host=127.0.0.1 port=${port}`
+  ).setState(Gst.State.PLAYING);
+  SkyWayStreamFactory.registerMediaDevices({ video: track });
+
   const room = await SkyWayRoom.Create(context, {
     type: 'sfu',
   });
   console.log('roomId', room.id);
   const sender = await room.join();
 
-  const video = await randomPort();
-  const onVideo = new Event<Buffer>();
-  createSocket('udp4')
-    .on('message', (buf) => {
-      onVideo.emit(buf);
-    })
-    .bind(video);
-
-  Gst.parseLaunch(
-    `videotestsrc ! video/x-raw,width=640,height=480,format=I420 ! x264enc key-int-max=60 ! rtph264pay ! udpsink host=127.0.0.1 port=${video}`
-  ).setState(Gst.State.PLAYING);
-
-  const track = new MediaStreamTrack({ kind: 'video' });
-  onVideo.add((data) => {
-    track.writeRtp(data);
-  });
-
-  const publication = await sender.publish(new LocalVideoStream(track));
+  const publication = await sender.publish(
+    await SkyWayStreamFactory.createCameraVideoStream()
+  );
 
   const receiver = await (await SkyWayRoom.Find(context, room, 'sfu')).join();
   const { stream: remoteStream } = await receiver.subscribe<RemoteVideoStream>(
